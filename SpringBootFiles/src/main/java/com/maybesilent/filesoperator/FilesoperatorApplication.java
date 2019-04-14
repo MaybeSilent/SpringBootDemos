02fc6d2c44ba347ff38141f6f2a1bd9e3a763912
MaybeSilent <804758962@qq.com>
2019-04-14 16:25:07 +0800

    init file operators

---
+++ SpringBootFiles/src/main/java/com/maybesilent/filesoperator/FilesoperatorApplication.java
@@ -0,0 +1,13 @@
+package com.maybesilent.filesoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesoperatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilesoperatorApplication.class, args);
+    }
+
+}
